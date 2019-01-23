8df7ee1e341253d15e385288107d0ca000124e8e
Adam Stone <a.stone2695@gmail.com>
2019-01-23 18:36:25 -0500

    Add update call to server

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, abort
 from config import Config
 from bitbucket_webhook_data import BitbucketWebhookData
+from build_indicator import BuildIndicator
 
 app = Flask(__name__)
 
 config = Config()
+build_indicator = BuildIndicator(config)
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -13,6 +15,7 @@ def webhook():
         print(data.repo_url)
         print(data.branch_name)
         print(data.build_status)
+        build_indicator.update_project_status(data.repo_url, data.build_status)
     return 'success', 200
 
 
