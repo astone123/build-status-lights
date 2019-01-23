7e5556b9791b770e82b55f11b695b1e57188d29c
Adam Stone <adam.stone@contrastsecurity.com>
2019-01-22 21:00:31 -0500

    Start doing some bitbucket webhook stuff

--- server.py
+++ server.py
@@ -1,16 +1,21 @@
 from flask import Flask, request, abort
+import yaml
 
 app = Flask(__name__)
 
+with open("config.yaml", 'r') as stream:
+    try:
+        PROJECTS = yaml.load(stream).get('projects', [])
+    except yaml.YAMLError as exc:
+        print(exc)
 
 @app.route('/', methods=['POST'])
 def webhook():
-	if request.method == 'POST':
-		print(request.json)
-		print(request.form)
-		return 'success', 200
-	else:
-		abort(400)
+	print(request.json)
+	data = request.get_json()
+	print(data.get('repository', {}).get('links', {}).get('html', {}).get('href', {}))
+	print(next(project for project in PROJECTS if project["repo_url"] == data.get('repository', {}).get('links', {}).get('html', {}).get('href', {})))
+	return 'success', 200
 
 
 if __name__ == '__main__':
