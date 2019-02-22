254358911d6f963c58b5670ea06f41e83e746e45
Donnie Propst <donald.propst@contrastsecurity.com>
2019-02-22 12:05:45 -0500

    print req data & 400 on bad requests

--- server.py
+++ server.py
@@ -8,15 +8,23 @@ app = Flask(__name__)
 config = Config()
 build_indicator = BuildIndicator(config)
 
+def inspect(r):
+    print('----------------------')
+    print(r.headers)
+    print(r.data)
+    print('----------------------')
+
 @app.route('/', methods=['POST'])
 def webhook():
     data = BitbucketWebhookData(request.get_json(), config)
+    inspect(request)
     if data.is_valid_repo_url and data.is_valid_branch:
         print(data.repo_url)
         print(data.branch_name)
         print(data.build_status)
         build_indicator.update_project_status(data.repo_url,data.branch_name, data.build_status)
-    return 'success', 200
+        return 'success', 200
+    return 400
 
 
 if __name__ == '__main__':
